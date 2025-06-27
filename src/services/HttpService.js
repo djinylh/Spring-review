6481495ee48b1586d10dc77cdd535b0ad16ee3d9
djinylh <d.jinylh@gmail.com>
2025-06-27 18:22:24 +0900

    복습 day1

---
+++ src/services/HttpService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080/api';
+
+class HttpService {
+  constructor() {
+    console.log('HttpService 객체 생성');
+  }
+
+  async postMemo(item) {
+    console.log('postMemo-item: ', item);
+  }
+
+  async getMemoList(params) {
+    // get메소드 호출 시 2번째 인자는 객체를 보내고
+    // 객체의 params 속성에 객체를 보내면
+    // 자동으로 쿼리스트링으로 변환해준다.
+    console.log('getMemoList-params: ', params);
+    const res = await axios.get('/memo', { params });
+    console.log('getMemoList - res.date: ', res.data);
+    return res.data;
+  }
+  async getMemo(id) {
+    console.log('getMemo-id : ', id);
+  }
+  async putMemo(item) {
+    console.log('putMemo - item : ', item);
+  }
+  async deleteMemo(params) {
+    console.log('deleteMemo - params: ', params);
+    const res = await axios.delete('/memo', { params });
+    return res.data;
+  }
+}
+
+export default new HttpService();
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { reactive, onMounted } from 'vue';
+import httpService from '@/services/HttpService';
+import MemoCard from '@/components/MemoCard.vue';
+//반응형 변수
+const state = reactive({
+  memos: [],
+});
+
+onMounted(() => {
+  getItems({});
+});
+
+const getItems = async (param) => {
+  const data = await httpService.getMemoList(param);
+  state.memos = data.resultData;
+  console.log('state.memos :', state.memos);
+};
+</script>
+
+<template>
+  <div class="memo-list">
+    <MemoCard v-for="m in state.memos" :item="m" :key="m.id" />
+    <router-link to="/memos/add" class="add btn btn-light">
+      + 추가하기
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// .memo-list {
+//   .item {
+//     background-color: #f8f9fa;
+//     border: 1px solid #eee;
+//     display: block;
+//     color: #000;
+//     text-decoration: none;
+//     padding: 20px 30px;
+//     margin: 15px 0;
+//     &:hover {
+//       border-color: #aaa;
+//     }
+//   }
+// }
+.add {
+  display: block;
+  padding: 25px;
+  border: 1px solid #eee;
+  text-align: center;
+}
+</style>
